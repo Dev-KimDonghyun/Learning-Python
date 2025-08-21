2b8d34184557f4b35b083130475b30361032ad7a
Dev-KimDonghyun <hyungus723@gmail.com>
2025-08-21 09:20:36 +0900

    learn: Add Python File for File I/O Learn

---
+++ Python Basic/11_file/read.py
@@ -0,0 +1,5 @@
+# 2025.08.21
+
+with open('example.txt', 'r') as F :
+    text_data = F.read()
+    print(text_data)
---
+++ Python Basic/11_file/write.py
@@ -0,0 +1,5 @@
+# 2025.08.21
+
+with open('example.txt', 'w') as F :
+    F.write("Hello World! \n")
+    F.write("Welcome to The Python Learning \n")
